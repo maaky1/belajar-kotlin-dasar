341c01afa7f45dc6012969899782b4fdd97e4f58
M Agus F <aagusfikry1@gmail.com>
2024-12-22 12:51:47 +0700

    Learn operator matematika

---
+++ src/OperatorMatematika.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var result = 10.0 / 3
+    println(result)
+
+    var result2 = 10 + 10 / 2
+    println(result2)
+
+    var total: Int = 0
+    val barang1 = 100
+    total += barang1
+    val barang2 = 200
+    total += barang2
+    val barang3 = 300
+    total += barang3
+    println(total)
+
+    val suhu = -30
+    println(suhu)
+
+    val sehat = true
+    println(!sehat)
+}
