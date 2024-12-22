f15d5aeedae074d084e8e5a6a12f48ed4ce73cbb
M Agus F <aagusfikry1@gmail.com>
2024-12-22 12:42:28 +0700

    Learn String

--- src/String.kt
+++ src/String.kt
@@ -1,7 +1,6 @@
 fun main() {
     var firstName: String = "Muhamad"
     var lastName: String = "Agus Fikry"
-    var fullName: String = "Muhamad Agus Fikry"
     var address: String = """
        Jl Cibogo Bawah 25 D, 
        Bandung,
@@ -9,7 +8,6 @@ fun main() {
     """
     println(firstName)
     println(lastName)
-    println(fullName)
     println(address)
 
     var address1: String = """
@@ -24,4 +22,12 @@ fun main() {
     """.trimMargin(">")
     println(address1)
     println(address2)
+
+    var fullName1: String = firstName + " " + lastName
+    println(fullName1)
+
+    var fullName: String = "$firstName $lastName"
+    var desc: String = "$fullName length is ${fullName.length}"
+    println(fullName)
+    println(desc)
 }
